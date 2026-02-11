88df7abed62c690f4fad31f877ce9415fc6061f7
Vijay Shankar <vijayshankarmb@gmail.com>
2026-02-11 09:48:49 +0530

    add db

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "./lib/mongodb"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
